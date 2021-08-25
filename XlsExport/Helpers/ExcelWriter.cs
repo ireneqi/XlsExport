5b957c5cc71810da332f6697dc0e4da9019b2f95
SunYubo <sunyubo.alisa@outlook.com>
2021-08-25 09:51:48 +0800

    添加接口

---
+++ XlsExport/Helpers/ExcelWriter.cs
@@ -0,0 +1,17 @@
+﻿using NPOI.HSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XlsExport.Interfaces;
+
+namespace XlsExport.Helpers
+{
+    class ExcelWriter : IExcelWriter
+    {
+        private HSSFWorkbook _sheets;
+
+
+    }
+}
---
+++ XlsExport/Interfaces/IExcelWriter.cs
@@ -0,0 +1,8 @@
+﻿namespace XlsExport.Interfaces
+{
+    interface IExcelWriter
+    {
+        void WriteText(string text,int rowIndex,int colIndex);
+        void WriteLine();
+    }
+}
